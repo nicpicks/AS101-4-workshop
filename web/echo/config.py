f3b78c0d2fa9d731858b5e4363834604cfd6bc82
nicpicks <wychia21@gmail.com>
2024-06-20 14:30:51 +0800

    fix: hawkeye scanned vulnerabilities

--- web/echo/config.py
+++ web/echo/config.py
@@ -2,11 +2,9 @@ import os
 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "changeme"
 DEBUG = os.environ.get('DEBUG', False)
 DB_NAME = "postgres"
 DB_USER = "postgres"
-DB_PASS = "postgres"
 DB_SERVICE = "postgres"
 DB_PORT = "5432"
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
